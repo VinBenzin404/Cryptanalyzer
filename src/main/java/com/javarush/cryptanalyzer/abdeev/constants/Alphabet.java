b6aaddea3db4efbb792f47a230fef0e93772abd4
VinBenzin404 <121306910+VinBenzin404@users.noreply.github.com>
2023-03-04 18:14:12 +0500

    alphabet class added

---
+++ src/main/java/com/javarush/cryptanalyzer/abdeev/constants/Alphabet.java
@@ -0,0 +1,10 @@
+package com.javarush.cryptanalyzer.abdeev;
+
+public class Alphabet {
+    public static final String LETTERS_UPPER_CASE = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    public static final String LETTERS_LOWER_CASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    public static final String DIGITS = "0123456789";
+    public static final String SYMBOLS = ".,\":-!? ";
+
+    public static final String ALPHABET = LETTERS_UPPER_CASE+LETTERS_LOWER_CASE+DIGITS+SYMBOLS;
+}
